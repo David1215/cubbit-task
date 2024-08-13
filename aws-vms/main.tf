dbdba0cf020a26a1d3d6d9f7bb49a4753470b3e8
Davide Zanella <zanelladavide95@gmail.com>
2024-08-13 17:08:49 +0200

    add helm chart and main.tf file

--- aws-vms/main.tf
+++ aws-vms/main.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 # Define a variable for the AMI ID
 variable "ami_id" {
-  default = "ami-0be3bc68751c2dcf4" # Change this to your desired AMI
+  default = "ami-00060fac2f8c42d30" # Change this to your desired AMI
 }
 
 # Create a security group that allows SSH and HTTP access
@@ -36,6 +36,13 @@ resource "aws_security_group" "allow_ssh_http" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # Create EC2 instances
@@ -46,12 +53,24 @@ resource "aws_instance" "cub_task" {
   security_groups = [aws_security_group.allow_ssh_http.name]
   user_data = <<EOF
 #!/bin/bash
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
 curl -sfL https://get.k3s.io | sh -
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+cp /etc/rancher/k3s/k3s.yaml /home/ec2-user/k3s.yaml
+chown ec2-user:ec2-user /home/ec2-user/.kubeconfig
 EOF
 
   tags = {
     Name = "cub_task-instance-${count.index + 1}"
   }
+}
+
+resource "aws_ecr_repository" "go-app-registry" {
+  name                 = "go-app"
+  image_tag_mutability = "IMMUTABLE"
+  force_delete         = true
 }
 
 output "instance_public_ips" {
